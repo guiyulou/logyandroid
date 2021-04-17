cfeae4e25d21a8985e048deed423ffa980d9b633
fishlgy <30583087+fishlgy@users.noreply.github.com>
2021-04-18 01:53:31 +0800

    inject viewmodel in splash activity

--- app/src/main/java/com/me/louguiyu/ui/SplashActivity.kt
+++ app/src/main/java/com/me/louguiyu/ui/SplashActivity.kt
@@ -3,10 +3,13 @@ package com.me.louguiyu.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.me.louguiyu.R
+import com.me.louguiyu.data.viewModel.SplashViewModel
 import com.me.louguiyu.databinding.ActivitySplashBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SplashActivity: AppCompatActivity() {
     private lateinit var binding: ActivitySplashBinding
+    val viewModel: SplashViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
