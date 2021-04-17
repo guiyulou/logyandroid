c98560c4dffae5953994d34188c342f57e4c59fd
fishlgy <30583087+fishlgy@users.noreply.github.com>
2021-04-17 23:36:47 +0800

    add timbre gradle for logging

---
+++ app/src/main/java/com/me/louguiyu/FishApplication.kt
@@ -0,0 +1,15 @@
+package com.me.louguiyu
+
+import android.app.Application
+import timber.log.Timber
+
+class FishApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
+}
