d759347d7c641313d0cdf90e5f1fbc7d32bc4c9d
fishlgy <30583087+fishlgy@users.noreply.github.com>
2021-04-18 00:11:24 +0800

    update enable view binding

--- app/src/main/java/com/me/louguiyu/ui/SplashActivity.kt
+++ app/src/main/java/com/me/louguiyu/ui/SplashActivity.kt
@@ -3,8 +3,11 @@ package com.me.louguiyu.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.me.louguiyu.R
+import com.me.louguiyu.databinding.ActivitySplashBinding
 
 class SplashActivity: AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
